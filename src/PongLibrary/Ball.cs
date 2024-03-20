5b894abd9bcaf91d4eaa443c20642313d0acca7c
mikeirvingjr <84804662+mikeirvingjr@users.noreply.github.com>
2024-03-19 21:22:59 -0400

    Add project files.

---
+++ src/PongLibrary/Ball.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PongLibrary;
+
+public class Ball
+{
+    public int X { get; set; }
+
+    public int Y { get; set; }
+
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public Color Color { get; set; }
+
+    private int _deltaX;
+
+    private int _deltaY;
+
+    public void Reset()
+    {
+        Random random = new();
+
+        // X = VIRTUAL_WIDTH / 2 - 2;
+        // Y = VIRTUAL_HEIGHT / 2 - 2;
+
+        _deltaX = random.Next(2) == 1 ? -100 : 100;
+        _deltaY = random.Next(-50, 50);
+    }
+
+    public void Update(float deltaTime)
+    {
+        X += (int)(_deltaX * deltaTime);
+        Y += (int)(_deltaY * deltaTime);
+    }
+
+    public Ball(int x, int y, int width, int height, Color color)
+    {
+        X = x;
+        Y = y;
+        Width = width; 
+        Height = height;
+        Color = color;
+    }
+}
---
+++ src/PongLibrary/Paddle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongLibrary;
+
+public class Paddle
+{
+    #region Properties
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+
+    public int DeltaY { get; set; }
+
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public Color Color { get; set;  }
+
+    public bool UseAI { get; set;  }
+
+    public int Score { get; set; }
+
+    #endregion
+
+    #region Methods
+
+    public void Update(GameTime gameTime)
+    {
+        if (DeltaY < 0)
+            Y = (int)Math.Max(0, Y + DeltaY * gameTime.ElapsedGameTime.TotalSeconds);
+        else
+            Y = (int)Math.Min(/*VIRTUAL_HEIGHT*/ -Height, Y + DeltaY * gameTime.ElapsedGameTime.TotalMilliseconds);
+    }
+
+    public void UpdateScore(int amount) => Score += amount;
+
+    public void ResetScore() => Score = 0;
+
+    public void ToggleAI() => UseAI = !UseAI;
+
+    public void Render(SpriteBatch spriteBatch)
+    { 
+        //Texture2D rectangle = new Texture2D(GraphicsDevice, 1, 1);
+        //rectangle.SetData(new[] { Color.White });
+
+        //// rectangle.Dispose(); in UnloadContent
+
+        //spriteBatch.Draw()
+    }
+
+    #endregion
+
+    public Paddle(int x, int y, int width, int height, Color color, bool useAI = false)
+    {
+        X = x;
+        Y = y; 
+        Width = width; 
+        Height = height; 
+        Color = color; 
+        UseAI = useAI;
+        DeltaY = 0;
+        Score = 0;
+    }
+}
